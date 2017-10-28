f22ccf1df18712ecd0cd3871b08197eda6b0f422
1999praveena <praveenaarokiaraj@gmail.com>
2017-10-28 21:55:32 +0530

    Create beta.c

---
+++ beta.c
@@ -0,0 +1,17 @@
+#iclude<stdio.h>
+#include<conio.h>
+void main()
+{
+char a;
+printf("enter value");
+scanf("%d",&a);
+if(a>='a' && a<='z')
+{
+printf("alphabet");
+}
+else
+{
+printf("not alphabet");
+}
+getch();
+}
